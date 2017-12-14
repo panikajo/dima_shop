b30f5f2b18ae852c87f2e701d2ef266be7221d48
panikajo <panikajo@gmail.com>
2017-12-14 17:04:29 +0200

    add index.php add header.php add footer.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panikajo
+ * Date: 14.12.2017
+ * Time: 15:55
+ */
--- index.php
+++ index.php
@@ -16,5 +16,17 @@ while(!feof($name)) {
 $footer = file_get_contents("footer.php");
 echo $footer;
 
+$category = file_get_contents("db/category.txt");
+echo $category;
 
+?>
+
+<?php
+$file = 'db/category.txt';
+// Открываем файл для получения существующего содержимого
+$current = file_get_contents($file);
+// Добавляем нового человека в файл
+$current .= "John Smith\n";
+// Пишем содержимое обратно в файл
+file_put_contents($file, $current);
 ?>
