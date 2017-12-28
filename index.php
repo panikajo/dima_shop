76bfb0f839d983319040f285064c924efe53dbf9
panikajo <panikajo@gmail.com>
2017-12-28 11:56:50 +0200

    add index.php add header.php add footer.php

--- index.php
+++ index.php
@@ -6,17 +6,20 @@
  * Time: 16:01
  */
 
-$r = readfile("header.php");
+$r = readfile("./templates/header.php");
 $name = fopen("order.php", "r");
 while(!feof($name)) {
     echo fgets($name);
 }
+
+
 $c = readfile("category.php");
 $cat = file_get_contents("db/category.txt");
 $arr = explode("\n", $cat);
-foreach ($arr as $value => $key) {
-    echo $key."<br>";
+foreach ($arr as $value) {
+    $a = explode("|",$value);
+    echo $a[1]."<br/>";
 }
-$footer = file_get_contents("footer.php");
-echo $footer;
 
+$footer = file_get_contents("./templates/footer.php");
+echo $footer;
